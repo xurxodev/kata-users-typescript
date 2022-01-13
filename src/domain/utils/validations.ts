e42563844cde1a02c8f9710dcb90caaea2f27b3c
Jorge Sánchez <xurxosf@gmail.com>
2022-01-13 18:27:42 +0100

    Remove unnecessary code

--- src/domain/utils/validations.ts
+++ src/domain/utils/validations.ts
@@ -6,12 +6,6 @@ export function validateRequired(value: any): ValidationErrorKey[] {
     return isBlank ? ["field_cannot_be_blank"] : [];
 }
 
-export function validateRequiredNumber(value: number): ValidationErrorKey[] {
-    const isNonrequiredValue = !value || value <= 0;
-
-    return isNonrequiredValue ? ["field_number_must_be_greater_than_0"] : [];
-}
-
 export function validateRegexp(value: string, regexp: RegExp): ValidationErrorKey[] {
     return regexp.test(value) ? [] : ["invalid_field"];
 }
